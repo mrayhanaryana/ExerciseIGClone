9736857fcce3cd8af251a8351fdd042b64350287
mrayhanaryana <mrayhanaryana99@gmail.com>
2021-12-30 23:51:39 +0700

    new update

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,6 +1,37 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import { Divider } from 'react-native-elements'
+
+const postFootericons = [
+    {
+        name: 'Like',
+        imageUrl:
+            'https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/344/external-heart-miscellaneous-kiranshastry-lineal-kiranshastry.png' ,
+        likedimageUrl: 'https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/344/external-heart-miscellaneous-kiranshastry-lineal-color-kiranshastry.png',
+    },
+
+    {
+        name: 'Comment',
+        imageUrl:
+            'https://img.icons8.com/ios/344/topic.png' ,
+        
+    },
+
+    {
+        name: 'Share',
+        imageUrl:
+            'https://img.icons8.com/ios/2x/paper-plane.png' ,
+        
+    },
+
+    {
+        name: 'Save',
+        imageUrl:
+            'https://i.pinimg.com/originals/1a/77/b8/1a77b82fa5cf6252cb3169e29c613d85.png' ,
+        
+    },
+
+]
 
 
 const Post = ({post}) => {
@@ -8,17 +39,133 @@ const Post = ({post}) => {
         <View style={{marginBottom: 30}}>
             <Divider width={1} orientation='vertical' />
             <PostHeader post={post} />
+            <PostImage post={post} />
+            <View style={{marginHorizontal: 15, marginTop: 10}}>
+                <PostFooter />
+                <Likes post={post} />
+                <Caption post={post} />
+                <CommentsSection post={post} />
+                <Comments post={post} />
+            </View>
         </View>
     )
 }
 
 const PostHeader=({post})=>(
-    <View>
-        <View>
-            <Image />
-            <Text></Text>
+    <View 
+        style={{flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        margin: 5 , 
+        alignItems: 'center', 
+        }}
+    >
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Image source={{uri: post.profile_picture}} style={styles.story} />
+            <Text style={{color: 'black', marginLeft: 5, fontWeight: '700'}}>
+                {post.user}
+            </Text>
         </View>
+        <Text style={{color: 'black', fontWeight: '900'}}>...</Text>
     </View>
 )
+
+const PostImage = ({post})=>(
+    <View
+    style={{
+        width: '100%',
+        height: 450,
+        }}
+    >
+        <Image source={{uri: post.imageUrl}} style={{height: '100%', 
+        resizeMode: 'cover'}}
+        />
+    </View>
+)
+
+const PostFooter = () =>(
+    <View style={{flexDirection: 'row'}}>
+        <View style={styles.leftFooterIconsContainer}>
+            <Icon  imgStyle={styles.footerIcon} imgUrl={postFootericons[0].imageUrl} /> 
+            <Icon  imgStyle={styles.footerIcon} imgUrl={postFootericons[1].imageUrl} />
+            <Icon  imgStyle={styles.footerIcon} imgUrl={postFootericons[2].imageUrl} />
+        </View>
+
+        <View style={{flex: 1, alignItems: 'flex-end'}}>
+            <Icon imgStyle={styles.footerIcon} imgUrl={postFootericons[3].imageUrl} />
+        </View>
+    </View>
+
+
+)
+
+const Icon = ({imgStyle,imgUrl}) =>(
+    <TouchableOpacity>
+        <Image  style = {imgStyle} source={{uri: imgUrl}} />
+    </TouchableOpacity>
+)
+
+const Likes = ({post})=> (
+    <View style={{flexDirection: 'row', marginTop: 4}}>
+        <Text style={{color: 'black', fontWeight: '600'}}>{post.likes.toLocaleString('en')} likes</Text>
+    </View>
+    
+)
+
+const Caption = ({post}) =>(
+    <View style={{marginTop: 5}}>
+        <Text style={{color: 'black'}}>
+            <Text style={{fontWeight: '600'}}>{post.user}</Text> 
+            <Text> {post.caption}</Text> 
+        </Text>
+    </View>
+)
+
+const CommentsSection = ({post}) =>(
+<View style={{marginTop: 5}}>
+    {!! post.comments.length &&(
+        <Text style={{color: 'gray'}}>
+            View {post.comments.length > 1 ? 'all' : ''} {post.comments.length} {''}
+            {post.comments.length> 1 ? 'comments' : 'comment'}
+        </Text>
+    )}
+</View>
+)
+
+const Comments = ({post}) => (
+    <>
+        {post.comments.map((comment, index) => (
+            <View key={index} style={{flexDirection: 'row', marginTop: 5}}>
+                <Text style={{color: 'black'}}>
+                    <Text style={{fontWeight: '600'}}> {comment. user} </Text>
+                    {comment.comment}
+                </Text>
+            </View>
+    
+        ))}
+    </>
+   
+)
+
+const styles = StyleSheet.create({
+    story: {
+        width: 35,
+        height: 35,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 1.6,
+        borderColor: '#ff8501',
+    },
+
+    footerIcon: {
+        width: 33,
+        height: 33,
+    },
+
+    leftFooterIconsContainer: {
+        flexDirection: 'row',
+        width: '32%',
+        justifyContent: 'space-between',
+    }
+})
 
 export default Post
--- data/posts.js
+++ data/posts.js
@@ -3,15 +3,36 @@ import { USERS} from "./users";
 export const POSTS = [
     {
         imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Tom_Holland_by_Gage_Skidmore.jpg/800px-Tom_Holland_by_Gage_Skidmore.jpg',
-        user: USERS[0].user,
+        user: USERS[2].user,
         likes: 7000,
         caption: 'No Way Home :(',
-        profile_picture: USERS[0].image,
+        profile_picture: USERS[2].image,
         comments:[
             {
                 user:'Azmi',
                 comment:'Cant Waiting for this!'
             }
         ],
+    },
+
+    {
+        imageUrl:'https://static.zerochan.net/Ousama.Ranking.full.3467526.jpg',
+        user: USERS[1].user,
+        likes: 5000,
+        caption: 'DAISUKI!',
+        profile_picture: USERS[1].image,
+        comments:[
+            {
+                user:'Ariq',
+                comment:'Anone anone ^-^'
+            },
+
+            {
+                user:'mrayhanaryana',
+                comment:'cot'
+            },
+
+        ],
+        
     },
 ]
