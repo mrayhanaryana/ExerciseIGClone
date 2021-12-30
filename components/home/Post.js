d77b32069adbcb2c95fed6ad1bceb0c2b97f369c
mrayhanaryana <mrayhanaryana99@gmail.com>
2021-12-30 15:27:22 +0700

    new update

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { Divider } from 'react-native-elements'
 
 
-const Post = () => {
+const Post = ({post}) => {
     return (
         <View style={{marginBottom: 30}}>
             <Divider width={1} orientation='vertical' />
-            <Text style={{color: 'black'}}>POST</Text>
+            <PostHeader post={post} />
         </View>
     )
 }
+
+const PostHeader=({post})=>(
+    <View>
+        <View>
+            <Image />
+            <Text></Text>
+        </View>
+    </View>
+)
 
 export default Post
